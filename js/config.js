266738442758e60e46b273a081f646c92b78df6e
Kaan <105629060+Afoldus@users.noreply.github.com>
2026-01-04 21:40:42 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -50,13 +50,13 @@ Config.backgroundImage = "";
  * If provided, these will rotate on the background.
  */
 Config.backgroundImages = [
+  "7.png",
   "1.png",
   "2.png",
   "3.png",
   "4.png",
   "5.png",
   "6.png",
-  "7.png"
 ];
 
 /**
